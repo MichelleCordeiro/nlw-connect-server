3a5cd95ce60c53c4defdb7201ea05a02ef3af89e
Michelle Cordeiro <mic321@gmail.com>
2025-02-23 16:18:07 -0300

    feat: create initial route and documentation with fastify, zod and swagger

---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,36 @@
+import z from 'zod'
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+
+// body - search params(filtro paginaçã0), route params (identiifcar recursos c id)
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'Subscribes someone to the event',
+        tags: ['subscription'],
+        description: 'Subscribes someone to the event ...',
+        body: z.object({
+          name: z.string(),
+          email: z.string().email()
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string().email()
+          })
+        }
+      }
+    },
+    async (request, reply) => {
+      const { name, email } = request.body;
+
+      // criação da inscrição no bd
+      return reply.status(201).send({
+        name,
+        email
+      })
+    }
+  )
+}
